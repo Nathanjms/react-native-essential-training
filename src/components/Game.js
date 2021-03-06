ce27c9a7291a72d5251eef0ac1e0f8f3344df3e7
Nathan <nathanjms@outlook.com>
2021-03-06 14:42:13 +0000

    Styling and random number generation

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { StyleSheet, Text, View } from 'react-native';
 
 class App extends React.Component {
-    target = 10 + Math.floor(40 * Math.random());
+    static propTypes = {
+        randomNumberCount: PropTypes.number.isRequired,
+    };
+    randomNumbers = Array
+        .from({ length: this.props.randomNumberCount })
+        .map(() => 1 + Math.floor(10 * Math.random()));
+    target = this.randomNumbers
+        .slice(0, this.props.randomNumberCount - 2)
+        .reduce((acc, curr) => acc + curr, 0);
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.target}>{this.target}</Text>
+                <View style={styles.numberContainer}>
+                    {this.randomNumbers.map((randomNumber, i) =>
+                        <Text key={i} style={styles.number}>{randomNumber}</Text>
+                    )}
+                </View>
             </View>
         );
     }
@@ -19,11 +33,32 @@ const styles = StyleSheet.create({
     },
 
     target: {
-        fontSize: 40,
-        backgroundColor: '#aaa',
-        marginHorizontal: 50,
+        fontSize: 50,
+        backgroundColor: '#bbb',
+        margin: 50,
         textAlign: 'center',
+        borderRadius: 20
+    },
+
+    numberContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        marginHorizontal: 20,
+        marginTop: 30,
+
+    },
+
+    number: {
+        backgroundColor: '#999',
+        width: 100,
+        margin: 15,
+        textAlign: 'center',
+        fontSize: 35,
+        borderRadius: 20,
     }
+
 });
 
 export default App;
