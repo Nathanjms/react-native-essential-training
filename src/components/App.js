d06b35a48307b3a4f12b8d2d78fd1afc4b6ff96d
Nathan <nathanjms@outlook.com>
2021-03-06 13:59:30 +0000

    Initial build

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ddd',
+    flex: 1,
+  },
+});
+
+export default App;
